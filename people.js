98f3924189849ca3a8094a02180431c06fcc4ab0
AlessioMura <alessiomura03@gmail.com>
2024-10-29 13:49:28 +0100

    minor fixes

--- people.js
+++ people.js
@@ -1,5 +1,5 @@
 //Importa la tua funzione da names.js
-const names = require('./names.js');
+const {firstName, lastName} = require('./names.js');
 
 //Importa la tua funzione da hobbies.js
 const hobbies = require('./hobbies.js');
@@ -7,7 +7,8 @@ const hobbies = require('./hobbies.js');
 //Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies. 
 function getProperties() {
     return {
-        names, 
+        firstName,
+        lastName, 
         hobbies
     }
 }
